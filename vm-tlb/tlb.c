d7977440853c80ae33c57b4dcefee018e8b4d624
Brandon Yanofsky <byanofsky@me.com>
2021-08-27 04:03:58 +0000

    Fix

--- vm-tlb/tlb.c
+++ vm-tlb/tlb.c
@@ -4,17 +4,23 @@
 
 int main(int argc, char* argv[]) {
   if (argc != 4) {
-    fprintf(stderr, "usage: tlb <pagesize> <numpages> <numtrials>\n");
+    fprintf(stderr, "usage: tlb <pagesize> <num_pages> <numtrials>\n");
     return 0;
   }
   int pagesize = atoi(argv[1]);
-  int numpages = atoi(argv[2]);
+  int num_pages = atoi(argv[2]);
   int numtrials = atoi(argv[3]);
   
-  int jump = pagesize / sizeof(int);
-  int a_size = numpages * jump;
-  int a[a_size];
+  int ints_per_page = pagesize / sizeof(int);
+  int a_size = num_pages * ints_per_page;
+  int* a = (int*) malloc(sizeof(int) * a_size);
 
+  if (a == NULL) {
+    perror("malloc array failed");
+    return 0;
+  }
+
+  // Initialize array to 0s
   for (int i = 0; i < a_size; i++) {
     a[i] = 0;
   }
@@ -26,35 +32,40 @@ int main(int argc, char* argv[]) {
   gettimeofday(start_tv, NULL);
 
   for (int n = 0; n < numtrials; n++) {
-    for (int i = 0; i < a_size; i += jump) {
+    for (int i = 0; i < a_size; i += ints_per_page) {
       a[i] += 1;
     }
   }
  
   gettimeofday(end_tv, NULL);
 
-  long long total_accesses = (long long) numtrials * a_size;
+  // Access all array elements to prevent removal by compiler
+  int none = 0;
+  for (int i = 0; i < a_size; i++) {
+    none += a[i];
+  }
+  //printf("array total: %d\n", none);
+
+  long long total_accesses = (long long) numtrials * num_pages;
   long diff_sec = end_tv->tv_sec - start_tv->tv_sec;
   long diff_usec = end_tv->tv_usec - start_tv->tv_usec;
   long total_usec = (1000000 * diff_sec) + diff_usec;
   double usec_per_access = ((double) total_usec) / total_accesses;
   double nsec_per_access = usec_per_access * 1000;
-  double nsec_per_access_v2 = ((double) total_usec) / (total_accesses / 1000);
 
 
-  printf("numtrials: %d\n", numtrials);
-  printf("start sec: %ld, start us: %ld\n", start_tv->tv_sec, start_tv->tv_usec);
-  printf("end sec: %ld, end us: %ld\n", end_tv->tv_sec, end_tv->tv_usec);
-  printf("diff sec: %ld, diff us: %ld\n", diff_sec, diff_usec);
-  printf("total usec: %ld\n", total_usec);
-  printf("a_size: %d\n", a_size);
-  printf("total accesses: %lld\n", total_accesses);
-  printf("usec per access: %f\n", usec_per_access);
-  printf("nsec per access: %f\n", nsec_per_access);
-  printf("nsec per access_v2: %f\n", nsec_per_access_v2);
+  //printf("numtrials: %d\n", numtrials);
+  //printf("start sec: %ld, start us: %ld\n", start_tv->tv_sec, start_tv->tv_usec);
+  //printf("end sec: %ld, end us: %ld\n", end_tv->tv_sec, end_tv->tv_usec);
+  //printf("diff sec: %ld, diff us: %ld\n", diff_sec, diff_usec);
+  //printf("total usec: %ld\n", total_usec);
+  //printf("a_size: %d\n", a_size);
+  //printf("total accesses: %lld\n", total_accesses);
+  //printf("usec per access: %f\n", usec_per_access);
+  //printf("nsec per access: %f\n", nsec_per_access);
+  printf("num_pages: %d, nsec_per_access: %f\n", num_pages, nsec_per_access);
 
+  free(a);
   free(start_tv);
   free(end_tv);
-
-  printf("done\n");
 }
