e3c656e3080a21e5c3d99e12f6601d7610d3034b
Chen-Chen Yeh <ge96noj@mytum.de>
2023-06-03 13:43:39 +0200

    add hw

--- threads-api/main-race.c
+++ threads-api/main-race.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-
 #include "common_threads.h"
 
 int balance = 0;
+pthread_mutex_t lock; // Declare a mutex lock
 
 void* worker(void* arg) {
-    balance++; // unprotected access 
+    pthread_mutex_lock(&lock); // Acquire the lock
+    balance++; // protected access 
+    pthread_mutex_unlock(&lock); // Release the lock
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
+    pthread_mutex_init(&lock, NULL); // Initialize the mutex lock
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
-    balance++; // unprotected access
+    pthread_mutex_lock(&lock); // Acquire the lock
+    balance++; // protected access
+    pthread_mutex_unlock(&lock); // Release the lock
     Pthread_join(p, NULL);
+    pthread_mutex_destroy(&lock); // Destroy the mutex lock
     return 0;
 }
