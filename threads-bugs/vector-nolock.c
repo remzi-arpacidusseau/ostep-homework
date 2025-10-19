6589a80d01c677f1d4cd591a79c57f2b14ce2b57
Bruce Lowekamp <bruce@lowekamp.net>
2025-10-19 12:46:50 -0400

    use C11 atomic_fetch_add on non-x86 arch
    tested on mac and x86_64 linux

--- threads-bugs/vector-nolock.c
+++ threads-bugs/vector-nolock.c
@@ -9,6 +9,7 @@
 #include "main-header.h"
 #include "vector-header.h"
 
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386) || defined(_M_IX86)
 // taken from https://en.wikipedia.org/wiki/Fetch-and-add
 int fetch_and_add(int * variable, int value) {
     asm volatile("lock; xaddl %%eax, %2;"
@@ -17,6 +18,12 @@ int fetch_and_add(int * variable, int value) {
 		 :"memory");
     return value;
 }
+#else // C11 standard version
+#include <stdatomic.h>
+int fetch_and_add(int * variable, int value) {
+    return atomic_fetch_add((_Atomic int *)variable, value);
+}
+#endif
 
 void vector_add(vector_t *v_dst, vector_t *v_src) {
     int i;
